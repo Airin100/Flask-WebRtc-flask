a03b9c2ab957f77d39b8a029746f7c755c583476
Airin100 <airinakter114@gmail.com>
2025-07-29 23:59:09 +0600

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('signal')
+def signaling(message):
+    socketio.emit('signal', message, broadcast=True, include_self=False)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
